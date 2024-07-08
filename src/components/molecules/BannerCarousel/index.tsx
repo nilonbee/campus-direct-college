b47bee0aef0e4aa6ec02f210cff3e3a65d013544
Bhanuka Krish <bhanukakrish@gmail.com>
2024-07-08 10:33:09 +0530

    fix: banner change

--- src/components/molecules/BannerCarousel/index.tsx
+++ src/components/molecules/BannerCarousel/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { rootImagePath } from "@/utils/rootImagePath";
 
 export const BannerCarousel = () => {
   const settings = {
@@ -20,35 +21,35 @@ export const BannerCarousel = () => {
       <Slider {...settings} className="w-full object-cover overflow-hidden">
         <div>
           <img
-            src={"images/slider/slider01.jpg"}
+            src={rootImagePath("/Slider/slider01.jpg?tr=w-1900")}
             alt="banner1"
             className="w-full"
           />
         </div>
         <div>
           <img
-            src={"images/slider/slider02.jpg"}
+            src={rootImagePath("/Slider/slider02.jpg?tr=w-1900")}
             alt="banner2"
             className="w-full"
           />
         </div>
         <div>
           <img
-            src={"images/slider/slider03.jpg"}
+            src={rootImagePath("/Slider/slider03.jpg?tr=w-1900")}
             alt="banner3"
             className="w-full"
           />
         </div>
         <div>
           <img
-            src={"images/slider/slider04.jpg"}
+            src={rootImagePath("/Slider/slider04.jpg?tr=w-1900")}
             alt="banner4"
             className="w-full"
           />
         </div>
         <div>
           <img
-            src={"images/slider/slider05.jpg"}
+            src={rootImagePath("/Slider/slider05.jpg?tr=w-1900")}
             alt="banner5"
             className="w-full"
           />
